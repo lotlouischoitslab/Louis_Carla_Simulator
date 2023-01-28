44803244a9a033d4a92d349aa6b7d3ed387e3575
lotlouischoitslab <louis.s.cho@gmail.com>
2023-01-28 15:25:18 -0600

    add

--- PythonAPI/examples/louis_tutorial.py
+++ PythonAPI/examples/louis_tutorial.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import argparse
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -11,16 +12,32 @@ except IndexError:
     pass
 
 import carla
+import random
+import time
 
 actor_list = []
 
 try:
-    client = carla.Client('localhost',2000)
+    louis_computer_host = '127.0.0.1'
+    client = carla.Client(louis_computer_host,2000)
+    client.set_timeout(2.0)
+
+    world = client.get_world()
+    blueprint_library = world.get_blueprint_library()
+
+    bp = blueprint_library.filter('model3')[0] #filter the model by model3
+    print(bp)
+
+    spawn_point = random.choice(world.get_map().get_spawn_points()) #random spawn points
+
+    vehicle = world.spawn_actor(bp, spawn_point)
+    vehicle.set_autopilot(True) 
+
+
+
 finally:
     for actor in actor_list:
         actor.destroy()
     print('All cleaned up!')
 
 
-import random
-import time
