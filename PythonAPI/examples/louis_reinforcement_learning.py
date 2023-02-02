0d9e0e5b685ae80bf7c9462b51259bc6644882b8
lotlouischoitslab <louis.s.cho@gmail.com>
2023-02-02 00:56:52 -0600

    add

--- PythonAPI/examples/louis_reinforcement_learning.py
+++ PythonAPI/examples/louis_reinforcement_learning.py
@@ -3,6 +3,12 @@ import os
 import sys
 import numpy as np 
 import cv2 
+from collections import deque
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam 
+from keras.models import Model 
+
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -16,10 +22,31 @@ import carla
 import random
 import time
 
-SECONDS_PER_EPISODE = 10
-SHOW_PREVIEW = False 
-IM_WIDTH = 640 
-IM_HEIGHT = 480 
+
+SHOW_PREVIEW = False #Set show preview to False
+IM_WIDTH = 640   #Image Width
+IM_HEIGHT = 480  #Image Height
+SECONDS_PER_EPISODE = 10 #Seconds per episode
+REPLAY_MEMORY_SIZE = 5_000 #5000 replay memory size
+MIN_REPLAY_MEMORY_SIZE = 1_000 #Minimum replay memory size
+PREDICTION_BATCH_SIZE = 1 #Prediction batch size
+TRAINING_BATCH_SIZE = MINIBATCH // 4 
+UPDATE_TARGET_EVERY = 5 #At the end of how many we update the model
+MODEL_NAME = 'Xception' #Which type of model to use
+
+MEMORY_FRACTION = 0.8 #GPU I want to use 
+MIN_REWARD = -200 #Minimum reward 
+
+EPISODES = 100 #Number of episodes
+
+DISCOUNT = 0.99 #Discount Rate
+epsilon = 1 #Epsilon value
+EPSILON_DECAY = 0.95 #How much we are going to decay
+MIN_EPSILON = 0.001 
+
+AGGREGATE_STATS_EVERY = 10 
+
+
 
 class CarEnv:
     SHOW_CAM = SHOW_PREVIEW 
@@ -68,6 +95,7 @@ class CarEnv:
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0,brake=0.0))
 
         return self.front_camera 
+
     
     def collision_data(self,event):
         self.collision_hist.append(event) 
@@ -84,13 +112,13 @@ class CarEnv:
 
     def step(self,action):
         if action == 0:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=-1*self.STEER_AMT)) 
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=-1*self.STEER_AMT)) #set to full throttle and go straight
         elif action == 1:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=0))  
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=0)) #go right
         elif action == 2:
-            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=self.STEER_AMT))  
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=self.STEER_AMT))  #go left
 
-        v = self.vehicle.get_velocity() 
+        v = self.vehicle.get_velocity() #speed
         kmh = int(3.6*np.sqrt(v.x**2 + v.y**2 + v.z**2)) 
 
         if len(self.collision_hist) != 0:
@@ -98,10 +126,10 @@ class CarEnv:
             reward = -200 #BIG penalty 
         elif kmh < 50:
             done = False 
-            reward = -1 #minor penalty
+            reward = -1 #Minor penalty
         else:
             done = False 
-            reward += 1
+            reward = 1 #We will keep appending one to the total reward
         
         if self.episode_start + SECONDS_PER_EPISODE < time.time():
             done = True 
@@ -109,4 +137,83 @@ class CarEnv:
         return self.front_camera, reward, done,None 
 
 class DQAgent:
-    pass 
+    def __init__(self):
+        self.model = self.create_model()
+        self.target_model=  self.create_model()
+        self.target_model.set_weights(self.model.get_weights()) 
+
+        self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+        self.tensorboard = ModifiedTensorBoard(log_dir=f'logs/{MODEL_NAME}-{int(time.time())}')
+        self.target_update_counter = 0
+        self.graph = tf.get_default_graph() 
+
+        self.terminate = False #Flags for our thread
+        self.last_logged_episode = 0 #Keep track of our tensorboard
+
+        self.training_initialized = False #When we start running simulation
+
+    
+    def create_model(self):
+        base_model = Xception(weights=None, include_top=False,input_shape=(IM_HEIGHT,IMG_WIDTH,3))
+
+        x = base_model.output #base model output
+        x = GlobalAveragePooling2D()(x) #average pooling
+
+        predictions = Dense(3,activation='linear')(x) #3 options: left, straight, right
+        model = Model(inputs=base_model.input,outputs=predictions) 
+        model.compile(loss='mse',optimizer=Adam(lr=0.001),metrics=['accuracy']) #measure the accuracy
+        return model #return the model
+
+    
+    def update_replay_memory(self,transition):
+        # transition = (current_state, action, reward, new_state, done)
+        self.replay_memory.append(transition) #replay the memory
+
+    def train(self):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return 
+        
+        minibatch = random.sample(self.replay_memory,MINIBATCH_SIZE) #batch of replay memory
+
+        current_states = np.array([transition[0] for transition in minibatch])/ 255 #scale the information
+
+        with self.graph.as_default():
+            current_qs_list = self.model.predict(current_states,PREDICTION_BATCH_SIZE)
+
+        new_current_states = np.array([transition[3] for transition in minibatch])/ 255 #scale the information
+
+        with self.graph.as_default():
+            future_qs_list = self.target_model.predict(new_current_states,PREDICTION_BATCH_SIZE)
+
+        X = []
+        y = [] 
+
+        for index,(current_state, action, reward, new_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT*max_future_q
+            else:
+                new_q = reward 
+        
+            current_qs = current_qs_list[index] #update the current q
+            current_qs[action] = new_q #update the actual q-value
+
+            X.append(current_state) #append the current state
+            y.append(current_qs) #append the current qs
+        
+        log_this_step = False 
+        if self.tensorboard.step > self.last_logged_episode:
+            log_this_step = True 
+            self.last_logged_episode = self.tensorboard.step 
+
+            
+        with self.graph.as_default():
+            self.model.fit(np.array(X)/255,np.array(y),batch_size=TRAINING_BATCH_SIZE,verbose=0,shuffle=False,callbacks=[self.tensorboard] if log_this_step else None )
+
+        if log_this_step:
+            self.target_update_counter += 1 
+        
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0 
