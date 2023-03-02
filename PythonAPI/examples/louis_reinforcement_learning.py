e69a99d4dc6ea32c8fd994043d5ddd3d7a68f831
lotlouischoitslab <louis.s.cho@gmail.com>
2023-03-01 22:17:31 -0600

    add

--- PythonAPI/examples/louis_reinforcement_learning.py
+++ PythonAPI/examples/louis_reinforcement_learning.py
@@ -54,22 +54,61 @@ class CarEnv:
         transform = carla.Transform(carla.Location(x=2.5,z=0.7))
         self.sensor = self.world.spawn_actor(self.rgb_cam,transform,attach_to=self.vehicle)
         self.actor_list.append(self.sensor)
-        #self.sensor.listen(lambda data: self.process_img(data))
-        
-
+        self.sensor.listen(lambda data: self.process_img(data))
 
         self.vehicle.apply_control(carla.VehicleControl(throttle=0.0,brake=0.0)) #apply control
         time.sleep(4) 
 
         colsensor = self.blueprint_library.find('sensor.other.collision')
-        self.colsensor = self.world.
+        self.colsensor = self.world.spawn_actor(colsensor,transform,attach_to=self.vehicle)
+        self.actor_list.append(self.colsensor)
+        self.colsensor.listen(lambda event: self.collision_data(event))
 
+        while self.front_camera is None:
+            time.sleep(0.01)
 
-def process_img(self,image):
-    i = np.array(image.raw_data) #convert the image into numpy array
-    i2 = i.reshape((self.im_height,self.im_width,4)) #reshape the image
-    i3 = i2[:,:,:3] #Entire height, entire width, rgba values
-    if self.SHOW_CAM:
-        cv2.imshow('',i3) #Display the image 
-        cv2.waitKey(1) 
-    self.front_camera =  i3/255.0 
+        self.episode_start = time.time()
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0,brake=0.0))
+
+        return self.front_camera 
+
+    def collision_data(self,event):
+        self.collision_hist.append(event) 
+    
+
+    def process_img(self,image):
+        i = np.array(image.raw_data) #convert the image into numpy array
+        i2 = i.reshape((self.im_height,self.im_width,4)) #reshape the image
+        i3 = i2[:,:,:3] #Entire height, entire width, rgba values
+        if self.SHOW_CAM:
+            cv2.imshow('',i3) #Display the image 
+            cv2.waitKey(1) 
+        self.front_camera =  i3/255.0 
+    
+    def step(self,action):
+        if action == 0:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=-1*self.STEER_AMT)) 
+        elif action == 1:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=0))  
+        elif action == 2:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0,steer=self.STEER_AMT)) 
+
+        v = self.vehicle.get_velocity() 
+        kmh = int(3.6*np.sqrt(v.x**2 + v.y**2 + v.z**2)) #kilometers per hour 
+
+        if len(self.collision_hist) != 0:
+            done = True 
+            reward = -200 
+        
+        elif kmh < 50:
+            done = False 
+            reward = -1 
+        
+        else:
+            done = False 
+            reward = 1 
+        
+        if self.episode_start + SECONDS_PER_EPISODE < time.time():
+            done = True 
+        
+        return self.front_camera, reward, done, None 
